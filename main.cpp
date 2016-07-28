96cac27f1265ae7909a37727a3c7e9fd8a352b83
cgugas <cgugas@harbinger.com>
2016-07-28 15:44:12 -0500

    added snark because reasons

--- main.cpp
+++ main.cpp
@@ -6,6 +6,14 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    if(argc > 1)
+    {
+        //damn kids....
+        cout<<"If you're passing command line arguments to Jerry-Rig, you are more than capable of running ffmpeg directly"<<endl<<endl;
+        cout<<"Aborting..."<<endl<<endl;
+        return -1;
+    }
+
     cerr<<"Starting Jerry-Rig"<<endl;
     QApplication a(argc, argv);
     MainMenu w;
